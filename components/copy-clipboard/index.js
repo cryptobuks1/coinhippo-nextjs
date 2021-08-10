1f7e6bbdea77a63fbef2ff28f47f8f089eaab053
Sirapop Na Ranong <sirapop@Sirapops-MacBook-Air.local>
2021-08-10 12:13:00 +0700

    remove unused

--- components/copy-clipboard/index.js
+++ components/copy-clipboard/index.js
@@ -1,4 +1,4 @@
-import { useState, useEffect, useCallback } from 'react'
+import { useState, useEffect } from 'react'
 import PropTypes from 'prop-types'
 import { CopyToClipboard } from 'react-copy-to-clipboard'
 import { FaRegCopy, FaCheckCircle } from 'react-icons/fa'
